e6f363ef70af20de65b08a41d7971080bceb2fde
ezmyzz <85902553+ezmyzz@users.noreply.github.com>
2021-08-12 16:36:45 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+TOKEN = "1909249918:AAEx93SlVoC3qMT81xD-UaWOADGQnKMTUko"
+exchanges = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+    'евро': 'EUR'
+}
